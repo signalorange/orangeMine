a08bd1b54813df834b017906c59d350a3828f04a
Jacob <lejacobroy@gmail.com>
2024-10-18 12:53:24 -0400

    add .env to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+source .env
 wget "https://gitlab.isc.org/isc-projects/kea/raw/Kea-$(echo "${KEA_VERSION}" | cut -d '.' -f 1).$(echo "${KEA_VERSION}" | cut -d '.' -f 2).$(echo "${KEA_VERSION}" | cut -d '.' -f 3)/src/share/database/scripts/pgsql/dhcpdb_create.pgsql" -O ./initdb/dhcpdb_create.sql
