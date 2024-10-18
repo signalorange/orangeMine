ad9522f140b6225bbcaf5b412618b40430180156
Jacob <lejacobroy@gmail.com>
2024-10-18 14:45:06 -0400

    add iptables

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source .env
+sudo iptables -I FORWARD -s ${SUBNET4} -j ACCEPT
+sudo iptables -I FORWARD -d ${SUBNET4} -j ACCEPT
 wget "https://gitlab.isc.org/isc-projects/kea/raw/Kea-$(echo "${KEA_VERSION}" | cut -d '.' -f 1).$(echo "${KEA_VERSION}" | cut -d '.' -f 2).$(echo "${KEA_VERSION}" | cut -d '.' -f 3)/src/share/database/scripts/pgsql/dhcpdb_create.pgsql" -O ./initdb/dhcpdb_create.sql
