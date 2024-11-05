3b14829a2b1def5cae03372bc48c5ad13e7ac66e
Jacob <lejacobroy@gmail.com>
2024-11-04 21:05:22 -0500

    scirpt must not exit

--- kea/config/start.sh
+++ kea/config/start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# Start the Kea DHCP server
-kea-dhcp4 -c /etc/kea/kea-dhcp4.conf &
+set -e
+
+# Your script logic here
+echo "Container is starting..."
+
+# You can still use CMD arguments
+exec "$@"
 
 # Start the Kea Control Agent
 kea-ctrl-agent -c /etc/kea/kea-ctrl-agent.conf &
 
 # Start the Stork Agent
 stork-agent --use-env-file --env-file=/etc/stork/agent.env &
+
+# Start the Kea DHCP server
+kea-dhcp4 -c /etc/kea/kea-dhcp4.conf
