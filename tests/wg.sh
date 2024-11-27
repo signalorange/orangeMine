abc45fa8744bdf6869bd4860412625dad6d8c558
Jacob <lejacobroy@gmail.com>
2024-11-27 12:16:46 -0500

    add wg test

--- tests/wg.sh
+++ tests/wg.sh
@@ -4,10 +4,10 @@ source .env
 TEST_IP="8.8.8.8"
 
 # Interface for WireGuard
-WG_INTERFACE="wg0"
+WG_INTERFACE=$(ip -br addr | grep "172.20.0.1" | awk '{print $1}')
 
 # Fallback interface (eth2)
-FALLBACK_INTERFACE="eth2"
+FALLBACK_INTERFACE=${ETH2}
 
 # Check if WireGuard VPN is up by pinging through wg0
 ping -I $WG_INTERFACE -c 2 $TEST_IP > /dev/null 2>&1
@@ -16,12 +16,18 @@ if [ $? -eq 0 ]; then
     # VPN is up, ensure default route through wg0 is set
     # ip route add default dev $WG_INTERFACE table wg_route 2>/dev/null
     echo "WG is up"
+    echo "Traceroute:"
     traceroute google.com
+    echo "IP:"
     curl ifconfig.me
+    echo "/n"
 else
     # VPN is down, remove WireGuard route and fall back to eth2
     # ip route del default dev $WG_INTERFACE table wg_route 2>/dev/null
     echo "WG is down"
+    echo "Traceroute:"
     traceroute google.com
+    echo "IP:"
     curl ifconfig.me
+    echo "/n"
 fi
